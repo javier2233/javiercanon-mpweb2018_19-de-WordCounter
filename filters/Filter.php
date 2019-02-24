301254a5db36a7e4e205d50b5c597cbb130dffd9
javiercanon <javiercanonguzman@gmail.com>
2019-02-24 17:49:10 -0500

    Agrega funcionalidad para filtros compuestos

--- filters/Filter.php
+++ filters/Filter.php
@@ -17,6 +17,15 @@ class Filter
 
         $key_words = $this->getKeyWords($text);
         $printer->printConsole($key_words);
+
+        $key_words = $this->vewelsTwoLetters($text);
+        $printer->printConsole($key_words);
+
+        $key_words = $this->keywordsVewel(true, false);
+        $printer->printConsole($key_words);
+
+        $key_words = $this->keywordsVewel(true, true);
+        $printer->printConsole($key_words);
     }
     public function getArrayWords ($text){
         $array_words = str_word_count($text,1, "áéíóúñ");
@@ -43,6 +52,18 @@ class Filter
         return $response;
     }
 
+    public function firstVocalByWord($word){
+
+        $response = false;
+        $firts = substr($word, 0, 1);
+        $firts = strtolower($firts);
+        $is_vewel = preg_match('/[aeiouáéóú]/', $firts);
+        if($is_vewel){
+            $response = true;
+        }
+        return $response;
+    }
+
     public function twoMoreChar($text){
         $words = $this->getArrayWords($text);
         $count = 0;
@@ -53,6 +74,15 @@ class Filter
             }
         }
         $response = "palabras con mas de 2 caracteres: ". $count;
+        return $response;
+    }
+
+    public function twoMoreCharByWord($word){
+        $response = false;
+        $chars = strlen($word);
+        if($chars > 2){
+            $response = true;
+        }
         return $response;
     }
 
@@ -67,6 +97,45 @@ class Filter
             }
         }
         $response = "Palabras clave: " . $counter;
+        return $response;
+    }
+
+    public function vewelsTwoLetters($text){
+        $words = $this->getArrayWords($text);
+        $counter = 0;
+        foreach ($words as $word){
+            $vewels = $this->firstVocalByWord($word);
+            $two_letters = $this->twoMoreCharByWord($word);
+            if($vewels && $two_letters){
+                $counter++;
+            }
+        }
+        $respose = "Contar solo las palabras que empiecen por vocal y tengan más de 2 caracteres: " . $counter;
+        return $respose;
+
+    }
+
+    public function keywordsVewel($is_vewels_first, $is_two_letters){
+        $words = $this->keywords;
+        $counter = 0;
+        $text_response = "";
+        foreach ($words as $word){
+            $firts = substr($word, 0, 1);
+            $firts = strtolower($firts);
+            $is_vewel = preg_match('/[aeiouáéóú]/', $firts);
+            if($is_vewel && $is_vewels_first){
+                if($is_two_letters){
+                    $text_response = "Contar solo palabras clave, que empiecen por vocal y tengan más de dos carácteres: ";
+                    if($this->twoMoreCharByWord($word)){
+                        $counter++;
+                    }
+                }else{
+                    $text_response = "Contar solo palabras clave que empiecen por vocal: ";
+                    $counter++;
+                }
+            }
+        }
+        $response = $text_response . $counter;
         return $response;
     }
 }
